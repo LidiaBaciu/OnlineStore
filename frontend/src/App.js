c5910e0cf011777de9b4aa7c4c658274b0612626
LidiaBaciu <lidia.baciu97@gmail.com>
2020-11-07 20:53:01 +0200

    my first contribution :100:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <>
-      <h1> Welcome </h1>
+      <h1> Hiii Alex, I just contributed the first time on an open source project :) </h1>
     </>
 
   );
