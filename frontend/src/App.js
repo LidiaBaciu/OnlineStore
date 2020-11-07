b46644acf5ec662833cbe5dfc9dd8e604e43dbea
AlexEnache97 <alex.enache97@yahoo.com>
2020-11-07 22:41:20 +0200

    Revert "Merge pull request #2 from LidiaBaciu/master"
    This reverts commit 7fdc71a8faf2bbbc65e062cd56933f2fc3fc8004, reversing
    changes made to bb75c548ab086200c2cfbc9b218c3e5bb113a782.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Header from './components/Header'
-import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -9,7 +7,6 @@ const App = () => {
     <h1> Hi Lidia, glad to hear it! :) </h1>
     <Footer />
     </>
-
   );
 }
 
