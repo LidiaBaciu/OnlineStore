9e4cb3eb3260c9523d0f2f2ead5f09f4cc3c0f78
LidiaBaciu <lidia.baciu97@gmail.com>
2020-11-08 21:02:15 +0200

    Connected to MongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config({ override: true })
+
+connectDB()
+
 const { NODE_ENV } = process.env
 
 const app = express()
