20158a01db23334851e6dccb8f978080fd58107d
LidiaBaciu <lidia.baciu97@gmail.com>
2020-11-09 13:27:53 +0200

    created redux store, product list reducer & action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,34 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
-
-//state of component level
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
-    
-    useEffect( () => {
-        //runs when the component loads
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            
-            setProducts(data)
-        }
 
-        fetchProducts()
-    }, [])
-    
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
+
+    useEffect( () => {
+        dispatch(listProducts())
+    }, [dispatch])
+
     return (
         <>
             <h1>Latest products</h1>
-            <Row>
+            {loading ? <h3>Loading...</h3> : error ? <h3>{error} </h3> : (<Row>
                 {products.map(product => (
                     <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
                        <Product product={product} />
                     </Col>
                 ))}
-            </Row>
+            </Row>)}
+            
         </>
     )
 }
