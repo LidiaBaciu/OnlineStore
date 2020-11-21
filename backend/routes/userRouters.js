5324a41e5b09e2369b68fdc559ca5c2136703143
LidiaBaciu <lidia.baciu97@gmail.com>
2020-11-21 20:32:07 +0200

    Admin middleware & get users endpoint

--- backend/routes/userRouters.js
+++ backend/routes/userRouters.js
@@ -1,10 +1,20 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import {
+	authUser,
+	registerUser,
+	getUserProfile,
+	updateUserProfile,
+	getUsers,
+} from '../controllers/userController.js'
+import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser)
+router.route('/').get(protect, isAdmin, getUsers)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router
+	.route('/profile')
+	.get(protect, getUserProfile)
+	.put(protect, updateUserProfile)
 
 export default router
