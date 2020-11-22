7e692f6f61aa2c2493f0a1fbb80eaec87d64b9c5
LidiaBaciu <lidia.baciu97@gmail.com>
2020-11-22 10:59:00 +0200

    Delete a user (admin only)

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -17,6 +17,9 @@ import {
 	USER_LIST_SUCCESS,
 	USER_LIST_FAIL,
 	USER_LIST_RESET,
+	USER_DELETE_SUCCESS,
+	USER_DELETE_REQUEST,
+	USER_DELETE_FAIL,
 } from '../constants/userConstants'
 import { ORDER_LIST_LOGGED_USER_RESET } from '../constants/orderConstants'
 import axios from 'axios'
@@ -171,8 +174,6 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
 			type: USER_LOGIN_SUCCESS,
 			payload: data,
 		})
-
-		localStorage.setItem('userInfo', JSON.stringify(data))
 	} catch (error) {
 		dispatch({
 			type: USER_UPDATE_PROFILE_FAIL,
@@ -206,11 +207,41 @@ export const listUsers = () => async (dispatch, getState) => {
 			type: USER_LIST_SUCCESS,
 			payload: data,
 		})
-
-		localStorage.setItem('userInfo', JSON.stringify(data))
 	} catch (error) {
 		dispatch({
 			type: USER_LIST_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		})
+	}
+}
+
+export const deleteUser = (id) => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: USER_DELETE_REQUEST,
+		})
+
+		const {
+			userLogin: { userInfo },
+		} = getState()
+
+		const config = {
+			headers: {
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		}
+
+		const { data } = await axios.delete(`/api/users/${id}`, config)
+
+		dispatch({
+			type: USER_DELETE_SUCCESS,
+		})
+	} catch (error) {
+		dispatch({
+			type: USER_DELETE_FAIL,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
