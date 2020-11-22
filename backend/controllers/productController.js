71ac26835915662d5225c4320edf85ba847ecf99
LidiaBaciu <lidia.baciu97@gmail.com>
2020-11-22 12:33:11 +0200

    Delete products (admin only)

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,31 +4,43 @@ import mongoose from 'mongoose'
 
 // @desc    Fetch all products
 // @route   GET /api/products
-// @access  Public 
-const getProducts = asyncHandler( async (req, res) => { 
-    const products = await Product.find({})
-    res.json(products)
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+	const products = await Product.find({})
+	res.json(products)
 })
 
 // @desc    Fetch a single product
 // @route   GET /api/products/:id
-// @access  Public 
-const getProductById = asyncHandler( async (req, res) => { 
-    if( mongoose.Types.ObjectId.isValid(req.params.id)) {
-        const product = await Product.findById(req.params.id)
-        if(product) {
-            res.json(product)
-        } else {
-            res.status(404)
-            throw new Error('Product not found')
-        }
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+	if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+		const product = await Product.findById(req.params.id)
+		if (product) {
+			res.json(product)
+		} else {
+			res.status(404)
+			throw new Error('Product not found')
+		}
+	} else {
+		res.status(404)
+		throw new Error('Product not found')
+	}
 })
 
-export {
-    getProducts,
-    getProductById,
-}
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id)
+	if (product) {
+		//trust any admin to delete any products
+		await product.remove()
+		res.json({ message: 'Product removed' })
+	} else {
+		res.status(404)
+		throw new Error('Product not found')
+	}
+})
+
+export { getProducts, getProductById, deleteProduct }
