ef5579441db35aa1a6cd221cf5d392f8001686bc
LidiaBaciu <lidia.baciu97@gmail.com>
2020-11-22 19:18:13 +0200

    Admin order List

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -13,6 +13,9 @@ import {
 	ORDER_LIST_LOGGED_USER_SUCCESS,
 	ORDER_LIST_LOGGED_USER_FAIL,
 	ORDER_LIST_LOGGED_USER_RESET,
+	ORDER_LIST_REQUEST,
+	ORDER_LIST_SUCCESS,
+	ORDER_LIST_FAIL,
 } from '../constants/orderConstants'
 
 export const orderCreateReducers = (state = {}, action) => {
@@ -104,6 +107,27 @@ export const orderListLoggedUserReducer = (state = { orders: [] }, action) => {
 		case ORDER_LIST_LOGGED_USER_RESET:
 			return {
 				orders: [],
+			}
+		default:
+			return state
+	}
+}
+
+export const orderListReducer = (state = { orders: [] }, action) => {
+	switch (action.type) {
+		case ORDER_LIST_REQUEST:
+			return {
+				loading: true,
+			}
+		case ORDER_LIST_SUCCESS:
+			return {
+				loading: false,
+				orders: action.payload,
+			}
+		case ORDER_LIST_FAIL:
+			return {
+				loading: false,
+				error: action.payload,
 			}
 		default:
 			return state
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -23,6 +23,7 @@ import {
 	orderDetailsReducers,
 	orderPayReducer,
 	orderListLoggedUserReducer,
+	orderListReducer,
 } from './reducers/orderReducers'
 
 const reducer = combineReducers({
@@ -43,6 +44,7 @@ const reducer = combineReducers({
 	orderDetails: orderDetailsReducers,
 	orderPay: orderPayReducer,
 	orderListLoggedUser: orderListLoggedUserReducer,
+	orderList: orderListReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
