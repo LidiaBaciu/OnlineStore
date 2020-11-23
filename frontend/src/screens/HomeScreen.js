8a5e601ace401f675b4524754338aa3277251880
LidiaBaciu <lidia.baciu97@gmail.com>
2020-11-23 09:29:33 +0200

    product search implementation

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,29 +6,35 @@ import Loader from '../components/Loader'
 import { useDispatch, useSelector } from 'react-redux'
 import { listProducts } from '../actions/productActions'
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+	const keyword = match.params.keyword
 
-    const dispatch = useDispatch()
-    const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+	const dispatch = useDispatch()
+	const productList = useSelector((state) => state.productList)
+	const { loading, error, products } = productList
 
-    useEffect( () => {
-        dispatch(listProducts())
-    }, [dispatch])
+	useEffect(() => {
+		dispatch(listProducts(keyword))
+	}, [dispatch, keyword])
 
-    return (
-        <>
-            <h1>Latest products</h1>
-    {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (<Row>
-                {products.map(product => (
-                    <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-                       <Product product={product} />
-                    </Col>
-                ))}
-            </Row>)}
-            
-        </>
-    )
+	return (
+		<>
+			<h1>Latest products</h1>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant="danger">{error}</Message>
+			) : (
+				<Row>
+					{products.map((product) => (
+						<Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
+		</>
+	)
 }
 
 export default HomeScreen
